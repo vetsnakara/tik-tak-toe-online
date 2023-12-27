21dd5ac41d35fc8eb1efacb05f835d56481cf0a4
Konstantin Arakantsev <k.arakantsev@bftcom.com>
2023-12-27 10:54:21 +0300

    init

---
+++ pages/hello.ts
@@ -0,0 +1,16 @@
+const x = 1;
+
+function hello() {
+  const v = "name";
+  return v;
+}
+
+class Hello {
+  x = 1;
+
+  constructor() {
+    this.x = 2;
+  }
+}
+
+console.log("x", x);
---
+++ pages/test.jsx
@@ -0,0 +1,3 @@
+export default function TestPage() {
+  return <div className="text-5xl text-blue-800">Test Page</div>;
+}
