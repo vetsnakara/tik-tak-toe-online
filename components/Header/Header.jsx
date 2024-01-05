e42a4e5b259ede6c774f316c1ab9b6f200b31412
Konstantin Arakantsev <k.arakantsev@bftcom.com>
2024-01-05 19:23:31 +0300

    game field layout

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 import logo from "./logo.svg";
 import { Profile } from "../Profile";
 import { ArrowDownIcon } from "./icons/ArrowDownIcon";
+import { Button } from "../uikit/Button";
 
 export function Header() {
   return (
     <header className="flex h-24 items-center px-8 bg-white shadow-lg">
       <Image alt="logo" src={logo} />
       <div className="w-px h-8 bg-slate-200 mx-4" />
-      <button className="w-44 bg-teal-600 hover:bg-teal-500 transition-colors text-white rounded-lg px-5 py-2 text-2xl leading-tight">
+
+      <Button className="w-44" variant="primary" size="lg">
         Играть
-      </button>
+      </Button>
 
       <button className="flex items-center text-teal-600 gap-2 ml-auto">
         <Profile />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,15 @@
 import { GameInfo, GameTitle } from "../components/Game";
+import { GameField } from "../components/Game/GameField";
 import { Header } from "../components/Header";
 
 export default function HomePage() {
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
-      <main className="pt-6 mx-auto max-w-[616px]">
+      <main className="pt-6 mx-auto w-max">
         <GameTitle />
         <GameInfo className="mt-4" />
+        <GameField className="mt-4" />
       </main>
     </div>
   );
