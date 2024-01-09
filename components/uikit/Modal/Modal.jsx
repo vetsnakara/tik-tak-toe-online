6c47fda35ac1b4c3e0dd95d7c108b4738d7ea82f
Konstantin Arakantsev <k.arakantsev@bftcom.com>
2024-01-09 17:40:32 +0300

    add modal

---
+++ components/uikit/Modal/Modal.jsx
@@ -0,0 +1,77 @@
+import { createPortal } from "react-dom";
+import clsx from "clsx";
+
+import { CrossLightIcon } from "./CrossLightIcon";
+
+/**
+ * @param {{
+ *   isOpen: boolean
+ *   onClose: Function
+ *   width: "md" | "full",
+ *   children,
+ * }} props
+ */
+export function Modal({
+    isOpen = false,
+    onClose,
+    width = "md",
+    className,
+    children,
+}) {
+    if (!isOpen) return null;
+
+    const classes = clsx(
+        "bg-white rounded min-h-[320px] mx-auto relative flex flex-col",
+        {
+            md: "max-w-[640px] w-full",
+            full: "mx-5",
+        }[width],
+        className,
+    );
+
+    const handleClick = (e) => {
+        const inModal = e.target.closest("[data-id='modal']");
+        if (inModal) return;
+        onClose();
+    };
+
+    const modal = (
+        <div
+            onClick={handleClick}
+            className="fixed inset-0 bg-slate-900/60 backdrop-blur pt-10 pb-10 z-10 overflow-auto"
+        >
+            <div className={classes} data-id="modal">
+                <button
+                    className="w-8 h-8 rounded flex items-center justify-center
+                   hover:bg-white/40 bg-white/10 absolute left-[calc(100%+12px)] transition-colors"
+                    onClick={onClose}
+                >
+                    <CrossLightIcon className="w-4 h-4 text-white" />
+                </button>
+                {children}
+            </div>
+        </div>
+    );
+
+    return createPortal(modal, document.getElementById("modals"));
+}
+
+Modal.Header = function ModalHeader({ className, children }) {
+    return (
+        <div className={clsx("px-6 pt-6 pb-4 text-2xl", className)}>
+            {children}
+        </div>
+    );
+};
+
+Modal.Body = function ModalHeader({ className, children }) {
+    return <div className={clsx("px-6", className)}>{children}</div>;
+};
+
+Modal.Footer = function ModalHeader({ className, children }) {
+    return (
+        <div className={clsx("mt-auto p-6 flex gap-4 justify-end", className)}>
+            {children}
+        </div>
+    );
+};
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -4,6 +4,8 @@ import { GameInfo, GameTitle, useGameState } from "../components/Game";
 import { GameField } from "../components/Game/GameField/GameField";
 import { Header } from "../components/Header";
 import { GameSymbol } from "../components/Game/GameSymbol";
+import { Modal } from "../components/uikit/Modal/Modal";
+import { Button } from "../components/uikit/Button";
 
 export default function HomePage() {
     const [playersCount] = useState(4);
@@ -26,6 +28,30 @@ export default function HomePage() {
 
             <main className="pt-6 mx-auto w-max">
                 <GameTitle playersCount={playersCount} />
+
+                <Modal
+                    isOpen={true}
+                    onClose={() => {
+                        console.log("close");
+                    }}
+                    width="md"
+                >
+                    <Modal.Header>Title</Modal.Header>
+                    <Modal.Body>
+                        <div className="my-4">
+                            Победитель{" "}
+                            <span className="text-teal-600">Username</span>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button size="md" variant="outline">
+                            Вернуться
+                        </Button>
+                        <Button size="md" variant="primary">
+                            Играть снова
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
 
                 <GameInfo
                     playersCount={playersCount}
